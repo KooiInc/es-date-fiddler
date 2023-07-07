c5119c180bfc303044a39a30464df5731c0e1fdf
Renzo Kooi <renzo.kooi@gmail.com>
2023-07-07 13:14:07 +0200

    Minor: styling/devIcon

--- Demo/index.js
+++ Demo/index.js
@@ -2,7 +2,6 @@
 const isDev = location.host.startsWith(`dev.`);
 const devMini = t => t ? `../Bundle/index.esm.min.js` : `../index.esm.js`;
 import $ from "https://kooiinc.github.io/JQL/Bundle/jql.min.js";
-if (isDev) { document.title = `#DEV ${document.title}`; }
 if (!/stackblitz/i.test(location)) { console.clear(); }
 const perfNow = performance.now();
 const DateX = isDev
@@ -10,6 +9,11 @@ const DateX = isDev
   : (await import("../Bundle/index.esm.min.js")).default;
 const { log, logTop } = logFactory(true);
 window.DateX = DateX;
+
+if (isDev) {
+  document.title = `##DEV## ${document.title}`;
+  $(`link[rel="icon"]`).replaceWith($.LINK.prop({href: `./demoDevIcon.png`, rel: `icon`}));
+}
 
 demoNdTest();
 
@@ -413,7 +417,7 @@ function styleIt() {
       content: '\\21BA';
      }`,
     `body {
-      margin-top: 2rem;
+      margin: 2rem auto auto 2rem;
       font: normal 14px/17px system-ui, verdana, arial;
      }`,
     `.head pre, .head div { font-weight: normal }`,
